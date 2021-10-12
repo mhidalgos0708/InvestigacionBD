95e381f50a79e66136a4d958e1f118d6ce0de1e1
mhidalgos0708 <66220491+mhidalgos0708@users.noreply.github.com>
2021-10-12 01:53:42 -0600

    Filtro del precio y las etiquetas

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -28,22 +28,37 @@ router.get('/home/shop/brand/:brand', async (req, res) => {
 
 router.get('/home/shop/price/:price', async (req, res) => {
     const filter = req.params.price;
-    const limits = filter.split(' - ');
-    const firstLimit = parseFloat(limits[0].replace('$',''));
-    const secondLimit = parseFloat(limits[1].replace('$',''));
     const filters = await Filter.findOne({section: "shop"}).lean();
-    const numberTotalResults = await Product.find({
-        $and: [
-           { price: { $gte: firstLimit } },
-           { price: { $lt: secondLimit } }
-        ]
-     }).sort({_id: 1}).count().lean();
-    const products = await Product.find({
-        $and: [
-           { price: { $gte: firstLimit } },
-           { price: { $lt: secondLimit } }
-        ]
-     }).limit(12).sort({_id: 1}).lean();
+    if(filter == "$250+") {
+        const limit = filter.replace(/[$+]/g,'');
+        const numberTotalResults = await Product.find({price: {$gte: limit}}).sort({_id: 1}).count().lean();
+        const products = await Product.find({price: {$gte: limit}}).limit(12).sort({_id: 1}).lean();
+        res.render('shop/shop', {filters, numberTotalResults, products});
+    } else {
+        const limits = filter.split(' - ');
+        const firstLimit = parseFloat(limits[0].replace('$',''));
+        const secondLimit = parseFloat(limits[1].replace('$',''));
+        const numberTotalResults = await Product.find({
+            $and: [
+                { price: { $gte: firstLimit } },
+                { price: { $lt: secondLimit } }
+            ]
+        }).sort({_id: 1}).count().lean();
+        const products = await Product.find({
+            $and: [
+                { price: { $gte: firstLimit } },
+                { price: { $lt: secondLimit } }
+            ]
+        }).limit(12).sort({_id: 1}).lean();
+        res.render('shop/shop', {filters, numberTotalResults, products});
+    }
+});
+
+router.get('/home/shop/tags/:tag', async (req, res) => {
+    const filter = req.params.tag;
+    const filters = await Filter.findOne({section: "shop"}).lean();
+    const numberTotalResults = await Product.find({tags: filter}).sort({_id: 1}).count().lean();
+    const products = await Product.find({tags: filter}).limit(12).sort({_id: 1}).lean();
     res.render('shop/shop', {filters, numberTotalResults, products});
 });
 
