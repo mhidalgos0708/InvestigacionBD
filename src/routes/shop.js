65779144fcbf5269c2f3f07c56e5d29ce0cb0315
mhidalgos0708 <66220491+mhidalgos0708@users.noreply.github.com>
2021-10-13 00:24:15 -0600

    Búsqueda de productos por nombre

--- src/routes/shop.js
+++ src/routes/shop.js
@@ -6,24 +6,24 @@ const Filter = require('../models/Filter');
 router.get('/home/shop', async (req, res) => {
     const filters = await Filter.findOne({section: "shop"}).lean();
     const products = await Product.find().sort({_id: 1}).lean();
-    const numberTotalResults = products.length;
-    res.render('shop/shop', {filters, numberTotalResults, products});
+    const totalNumberResults = products.length;
+    res.render('shop/shop', {filters, totalNumberResults, products});
 });
 
 router.get('/home/shop/category/:category', async (req, res) => {
     const filter = req.params.category;
     const filters = await Filter.findOne({section: "shop"}).lean();
     const products = await Product.find({category: filter}).sort({_id: 1}).lean();
-    const numberTotalResults = products.length;
-    res.render('shop/shop', {filters, numberTotalResults, products});
+    const totalNumberResults = products.length;
+    res.render('shop/shop', {filters, totalNumberResults, products});
 });
 
 router.get('/home/shop/brand/:brand', async (req, res) => {
     const filter = req.params.brand;
     const filters = await Filter.findOne({section: "shop"}).lean();
     const products = await Product.find({brand: filter}).sort({_id: 1}).lean();
-    const numberTotalResults = products.length;
-    res.render('shop/shop', {filters, numberTotalResults, products});
+    const totalNumberResults = products.length;
+    res.render('shop/shop', {filters, totalNumberResults, products});
 });
 
 router.get('/home/shop/price/:price', async (req, res) => {
@@ -32,8 +32,8 @@ router.get('/home/shop/price/:price', async (req, res) => {
     if(filter == "$250+") {
         const limit = filter.replace(/[$+]/g,'');
         const products = await Product.find({price: {$gte: limit}}).sort({_id: 1}).lean();
-        const numberTotalResults = products.length;
-        res.render('shop/shop', {filters, numberTotalResults, products});
+        const totalNumberResults = products.length;
+        res.render('shop/shop', {filters, totalNumberResults, products});
     } else {
         const limits = filter.split(' - ');
         const firstLimit = parseFloat(limits[0].replace('$',''));
@@ -44,8 +44,8 @@ router.get('/home/shop/price/:price', async (req, res) => {
                 { price: { $lt: secondLimit } }
             ]
         }).sort({_id: 1}).lean();
-        const numberTotalResults = products.length;
-        res.render('shop/shop', {filters, numberTotalResults, products});
+        const totalNumberResults = products.length;
+        res.render('shop/shop', {filters, totalNumberResults, products});
     }
 });
 
@@ -53,24 +53,32 @@ router.get('/home/shop/tags/:tag', async (req, res) => {
     const filter = req.params.tag;
     const filters = await Filter.findOne({section: "shop"}).lean();
     const products = await Product.find({tags: filter}).sort({_id: 1}).lean();
-    const numberTotalResults = products.length;
-    res.render('shop/shop', {filters, numberTotalResults, products});
+    const totalNumberResults = products.length;
+    res.render('shop/shop', {filters, totalNumberResults, products});
 });
 
 router.get('/home/shop/colors/:color', async (req, res) => {
     const filter = req.params.color;
     const filters = await Filter.findOne({section: "shop"}).lean();
     const products = await Product.find({color: filter}).sort({_id: 1}).lean();
-    const numberTotalResults = products.length;
-    res.render('shop/shop', {filters, numberTotalResults, products});
+    const totalNumberResults = products.length;
+    res.render('shop/shop', {filters, totalNumberResults, products});
 });
 
 router.get('/home/shop/size/:size', async (req, res) => {
     const filter = req.params.size;
     const filters = await Filter.findOne({section: "shop"}).lean();
     const products = await Product.find({size: filter}).sort({_id: 1}).lean();
-    const numberTotalResults = products.length;
-    res.render('shop/shop', {filters, numberTotalResults, products});
+    const totalNumberResults = products.length;
+    res.render('shop/shop', {filters, totalNumberResults, products});
+});
+
+router.get('/home/shop/search', async (req, res) => {
+    const { search } = req.query;
+    const filters = await Filter.findOne({section: "shop"}).lean();
+    const products = await Product.find({name: {$regex: search, $options: "$i"}}).sort({_id: 1}).lean();
+    const totalNumberResults = products.length;
+    res.render('shop/shop', {filters, totalNumberResults, products});
 });
 
 module.exports = router;
